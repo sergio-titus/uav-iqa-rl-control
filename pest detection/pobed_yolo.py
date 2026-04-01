cc7445dbc7198989dcdb3e2e62628a0c8f6ac776
Aglin Koffi Titus Sergio <sergioaglin85@gmail.com>
2026-04-01 11:50:53 +0300

    Update pobed_yolo.py

--- pest detection/pobed_yolo.py
+++ pest detection/pobed_yolo.py
@@ -17,7 +17,7 @@ def load_yaml_config(filename: str):
 assert (Path.cwd() / "configs").exists(), "configs/ folder missing in project root"
 
 def main():
-cfg = load_yaml_config("yolo.yaml")["yolo"]
+    cfg = load_yaml_config("yolo.yaml")["yolo"]
 
 SEED = cfg["seed"]
 random.seed(SEED)
